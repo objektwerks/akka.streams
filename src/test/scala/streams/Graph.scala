8eca75127fca0875ae8c620153ac80e56e7ee036
objektwerks <objektwerks@runbox.com>
2019-06-26 08:01:18 -0400

    playing with runnable graphs and the like

--- src/test/scala/streams/Graph.scala
+++ src/test/scala/streams/Graph.scala
@@ -5,7 +5,7 @@ import akka.stream.ClosedShape
 import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, RunnableGraph, Sink, Source, Zip}
 
 object Graph {
-  val source = Source(1 to 1000)
+  val source = Source(1 to 10)
   val incrementer = Flow[Int].map(_ + 1)
   val multiplier = Flow[Int].map(_ * 2)
   val sink = Sink.reduce[(Int, Int)]( (a, b) => (a._1 + a._2, b._1 + b._2) )
