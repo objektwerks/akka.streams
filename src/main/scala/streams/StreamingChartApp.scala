a2087c19c7495b788090356abf849930959bbb40
objektwerks <objektwerks@runbox.com>
2020-11-01 16:55:13 -0500

    add chart update scheduler'

--- src/main/scala/streams/StreamingChartApp.scala
+++ src/main/scala/streams/StreamingChartApp.scala
@@ -6,11 +6,24 @@ import javax.swing.{JFrame, WindowConstants}
 import javax.swing.UIManager._
 
 import org.jfree.chart.ChartPanel
+import org.jfree.data.time.{TimeSeries, TimeSeriesDataItem}
+import org.jfree.data.time.Millisecond
+
+import scala.concurrent.duration._
+import scala.language.postfixOps
+import scala.util.Random
+import akka.actor.ActorSystem
+import com.typesafe.config.ConfigFactory
 
 object StreamingChartApp {
   def main(args: Array[String]): Unit = {
+    implicit val system = ActorSystem.create("streaming-chart-app", ConfigFactory.load("app.conf"))
+    implicit val dispatcher = system.dispatcher
+
     val chart = StreamingChart()
+    val timeSeries = chart.timeSeries
     val chartPanel = new ChartPanel( chart.jFreeChart )
+
     EventQueue.invokeLater( new Runnable() {
       override def run(): Unit = {
         setLookAndFeel(getSystemLookAndFeelClassName)
@@ -23,5 +36,17 @@ object StreamingChartApp {
         frame.setVisible(true)
       }
     })
+
+    // Source.tick(1 second, 300 milli, Done).runForeach { addOrUpdate(timeSeries) }
+    val cancellable = system.scheduler.scheduleAtFixedRate(300 millis, 300 milli)( addOrUpdate(timeSeries) )
+    println(cancellable.toString)
+  }
+
+  def addOrUpdate(timeSeries: TimeSeries): Runnable = new Runnable() {
+    override def run(): Unit = {
+      val item = new TimeSeriesDataItem( new Millisecond(), Random.nextDouble())
+      timeSeries.addOrUpdate(item)
+      ()
+    }
   }
 }
