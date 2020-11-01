cb5ffb1008aaade36e029ee4499bca61dbe9432e
objektwerks <objektwerks@runbox.com>
2020-11-01 08:11:55 -0500

    added base swing app

---
+++ src/main/scala/streams/StreamingChartApp.scala
@@ -0,0 +1,18 @@
+package streams
+
+import java.awt.EventQueue
+
+import javax.swing.JFrame
+import javax.swing.UIManager._
+
+object StreamingChartApp {
+  def main(args: Array[String]): Unit = {
+    EventQueue.invokeLater( new Runnable() {
+      override def run(): Unit = {
+        setLookAndFeel(getSystemLookAndFeelClassName)
+        val frame = new JFrame()
+        frame.setVisible(true)
+      }
+    })
+  }
+}
