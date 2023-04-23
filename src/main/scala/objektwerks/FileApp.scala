258fb0416b3be4e500e87554000810b4d1886db2
objektwerks <objektwerks@runbox.com>
2023-04-23 08:17:23 -0400

    enhance file app

--- src/main/scala/objektwerks/FileApp.scala
+++ src/main/scala/objektwerks/FileApp.scala
@@ -1,21 +1,23 @@
 package objektwerks
 
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.scaladsl.{FileIO, Sink, Source}
 
 import com.typesafe.config.ConfigFactory
 
+import java.nio.file.Path
+
 import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.concurrent.duration._
-import scala.io.{Codec, StdIn}
+import scala.io.Codec
 import scala.language.postfixOps
 import scala.util.{Try, Using}
 
 object FileApp {
-  def words(): List[String] = {
-    Using( scala.io.Source.fromFile("./LICENSE", Codec.UTF8.name) ) { 
-      source => source.mkString.split("\\W+")
-    }.get.toList
+  def fileToLines(file: String): Try[Seq[String]] = {
+    Using( scala.io.Source.fromFile(file, Codec.UTF8.name) ) {
+      source => source.getLines().toSeq 
+    }
   }
 
   def main(args: Array[String]): Unit = {
@@ -23,13 +25,14 @@ object FileApp {
     implicit val dispatcher: ExecutionContext = system.dispatcher
     println("*** akka system started")
 
-
-    println(s"*** once all words have been printed, depress RETURN key to shutdown app")
-
-    StdIn.readLine()
+    val source = FileIO.fromPath(Path.of("./LICENSE"))
+    val sink = FileIO.toPath(Path.of("./target/license.txt"))
+    Await.result( source.runWith(sink), 10 seconds )
+    println("*** see new license file at /target/license.txt")
 
     Await.result(system.terminate(), 10 seconds)
     println("*** akka system terminated")
+
     println("*** see log at /target/app.log")
     println("*** app shutdown")
   }
