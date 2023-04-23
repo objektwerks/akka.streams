22899a2df1b240fbf7e73666e3a1980ae2dc625d
objektwerks <objektwerks@runbox.com>
2023-04-23 08:22:16 -0400

    edited file app

--- src/main/scala/objektwerks/FileApp.scala
+++ src/main/scala/objektwerks/FileApp.scala
@@ -20,7 +20,7 @@ object FileApp {
     val source = FileIO.fromPath(Path.of("./LICENSE"))
     val sink = FileIO.toPath(Path.of("./target/license.txt"))
     Await.result( source.runWith(sink), 2 seconds )
-    println("*** see new license file at /target/license.txt")
+    println("*** see license file at /target/license.txt")
 
     Await.result(system.terminate(), 2 seconds)
     println("*** akka system terminated")
