406d3cd35afc29d35c3f6e8cd0cd2a8e636c20db
objektwerks <objektwerks@runbox.com>
2023-04-24 08:56:14 -0400

    enhanced file app

--- src/main/scala/objektwerks/FileApp.scala
+++ src/main/scala/objektwerks/FileApp.scala
@@ -1,7 +1,7 @@
 package objektwerks
 
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{FileIO, Sink, Source}
+import akka.stream.scaladsl.{FileIO, Flow, Sink, Source}
 
 import com.typesafe.config.ConfigFactory
 
@@ -10,6 +10,7 @@ import java.nio.file.Path
 import scala.concurrent.{Await, ExecutionContext}
 import scala.concurrent.duration._
 import scala.language.postfixOps
+import akka.stream.scaladsl.Keep
 
 object FileApp {
   def main(args: Array[String]): Unit = {
@@ -20,6 +21,12 @@ object FileApp {
     val licenseFileSource = FileIO.fromPath(Path.of("./LICENSE"))
     val licenseFileSink = FileIO.toPath(Path.of("./target/copy.of.license.txt"))
     Await.result( licenseFileSource.runWith(licenseFileSink), 2 seconds )
+    println("*** see copy of license file at /target/copy.of.license.txt")
+
+    val numbersSource = Source(1 to 10)
+    val evensSquaredFlow = Flow[Int].filter(_ % 2 == 0).map(_ * 2)
+    val evensSquaredFileSink = FileIO.toPath(Path.of("./target/evens.squared.txt"))
+    Await.result( numbersSource.via(evensSquaredFlow).toMat(evensSquaredFileSink)(Keep.right).run(), 2 seconds )
     println("*** see copy of license file at /target/copy.of.license.txt")
 
     Await.result(system.terminate(), 2 seconds)
